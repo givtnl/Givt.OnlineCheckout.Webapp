7fce507660b9a9e7a4190c0d32ebbbbd33706f29
michiel46 <michielverkest@gmail.com>
2022-03-17 15:03:13 +0100

    static fromAmounts

--- src/app/setup-donation/model/organisation.ts
+++ src/app/setup-donation/model/organisation.ts
@@ -20,8 +20,16 @@ export class AmountData {
     this.id = id;
     this.value = value;
   }
+
+  static fromAmounts(amounts: number[]): AmountData[] {
+    let amountData: AmountData[] = [];
+    for (let i = 0; i < amounts.length; i++) {
+      amountData.push(new AmountData(i, amounts[i]))
+    }
+    return amountData
+  }
 }
 
 export const DATA = [
-  new Organisation(1, 'TestOrganisation', 'TestGoal', [new AmountData(1, 5), new AmountData(2, 10), new AmountData(3, 15)]),
+  new Organisation(1, 'TestOrganisation', 'TestGoal', AmountData.fromAmounts([3,15,27])),
 ];
