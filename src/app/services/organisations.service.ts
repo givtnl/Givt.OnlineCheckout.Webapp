a256ad6642c60f9de23d443dc0f76c6343c44f32
michiel46 <michielverkest@gmail.com>
2022-03-17 18:49:34 +0100

    how to do async await in typescript

--- src/app/services/organisations.service.ts
+++ src/app/services/organisations.service.ts
@@ -1,27 +1,30 @@
 import {HttpClient} from "@angular/common/http";
 import {IncomingOrganisation} from "../models/organisation";
 import {Injectable} from "@angular/core";
-import {Observable} from "rxjs";
+import {firstValueFrom, Observable} from "rxjs";
 
 @Injectable()
 export class OrganisationsService {
 
   constructor(private http: HttpClient) { }
 
-  getByMediumId(mediumId: string): Observable<any> {
-     return this.http.get<IncomingOrganisation>('http://localhost:5000/api/medium?mediumid='+mediumId)
+  async getByMediumId(mediumId: string): Promise<IncomingOrganisation> {
+    return await firstValueFrom(this.http.get<IncomingOrganisation>('http://localhost:5000/api/medium?mediumid='+mediumId))
   }
 
-  async postDonation(amount: number) {
-    let paymentMethodId = ""
-    await this.http.post('http://localhost:5000/api/donation/intent', {
+  async postDonation(amount: number): Promise<PaymentMethodId> {
+    return await firstValueFrom(this.http.post<PaymentMethodId>('http://localhost:5000/api/donation/intent', {
       "amount": amount,
       "medium": "61f7ed014e4c0121c005.c00000000001",
       'paymentMethod': 1
-    }).subscribe(res => {
-        var paymentMethodIdWrapped: { 'paymentMethodId': string } = res as { 'paymentMethodId': string };
-        return paymentMethodId = paymentMethodIdWrapped.paymentMethodId;
-      });
+    }));
   }
+}
 
+export class PaymentMethodId {
+  paymentMethodId: string;
+
+  constructor(paymentMethodId: string) {
+    this.paymentMethodId = paymentMethodId;
+  }
 }
