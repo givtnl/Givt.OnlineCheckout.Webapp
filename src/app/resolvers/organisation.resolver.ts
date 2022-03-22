f0de44bd785ade25e6544a20bb5bbf6e741edade
Bjorn Derudder <bjorn.derudder@gmail.com>
2022-03-22 11:04:20 +0100

    fix try catch

--- src/app/resolvers/organisation.resolver.ts
+++ src/app/resolvers/organisation.resolver.ts
@@ -1,18 +1,18 @@
-import {Injectable} from "@angular/core";
-import {ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot} from "@angular/router";
-import {IncomingOrganisation} from "../models/models";
-import {catchError, Observable, of} from "rxjs";
-import {HttpClient} from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from "@angular/router";
+import { IncomingOrganisation } from "../models/models";
+import { catchError, Observable, of } from "rxjs";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable()
 export class OrganisationResolver implements Resolve<IncomingOrganisation> {
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient, private router: Router) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IncomingOrganisation | any> | Promise<IncomingOrganisation> | IncomingOrganisation {
-    let mediumIdEncoded = route.queryParams['code'];
-    let mediumIdDecoded = atob(mediumIdEncoded);
-    localStorage.setItem('medium', mediumIdDecoded)
     try {
+      let mediumIdEncoded = route.queryParams['code'];
+      let mediumIdDecoded = atob(mediumIdEncoded);
+      localStorage.setItem('medium', mediumIdDecoded)
       return this.http.get<IncomingOrganisation>('http://localhost:5000/api/medium?mediumid=' + mediumIdDecoded)
         .pipe(catchError(async () => {
           await this.router.navigate(["/error"]);
