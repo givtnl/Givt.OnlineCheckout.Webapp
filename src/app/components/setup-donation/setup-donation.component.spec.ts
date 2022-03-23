83dd4c31ae705249530adf1bed9537770430448a
michiel46 <michielverkest@gmail.com>
2022-03-23 14:52:15 +0100

    testing... attention pls :)

--- src/app/components/setup-donation/setup-donation.component.spec.ts
+++ src/app/components/setup-donation/setup-donation.component.spec.ts
@@ -1,14 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SetupDonationComponent } from './setup-donation.component';
+import {RouterTestingModule} from "@angular/router/testing";
+import {ActivatedRoute} from "@angular/router";
+import {IncomingOrganisation} from "../../models/models";
 
 describe('SetupDonationComponent', () => {
   let component: SetupDonationComponent;
   let fixture: ComponentFixture<SetupDonationComponent>;
 
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SetupDonationComponent ]
+      declarations: [ SetupDonationComponent ],
+      imports: [RouterTestingModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: {
+                organisation: new IncomingOrganisation([1,2,3], "goal", "medium", "name", "ty", 1)
+              }
+            }
+          },
+        },
+      ]
+
     })
     .compileComponents();
   });
