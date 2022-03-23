19093fb53772feb2c5117ebd92ee758bb19e4a59
Nico H <nico@givtapp.net>
2022-03-23 15:18:40 +0100

    Thx Michiel

--- src/app/components/payment/payment.component.spec.ts
+++ src/app/components/payment/payment.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { StripeService } from 'ngx-stripe';
+import { IncomingOrganisation } from 'src/app/models/models';
 import { PaymentComponent } from './payment.component';
 
 describe('PaymentComponent', () => {
@@ -8,7 +11,22 @@ describe('PaymentComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PaymentComponent ]
+      declarations: [ PaymentComponent ],
+      imports: [RouterTestingModule, StripeService],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: {
+                donation: "" 
+              }
+            }
+          },
+        },
+      StripeService
+      ]
+
     })
     .compileComponents();
   });
