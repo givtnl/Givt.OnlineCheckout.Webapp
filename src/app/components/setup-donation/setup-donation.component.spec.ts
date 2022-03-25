bc370eb9d835074fd67052209727d3d46ddfdeb4
Nico H <nico@givtapp.net>
2022-03-25 14:57:39 +0100

    added 2 unit tests - thanks again, Michiel :-) what a dot and a right name can do to a test, hey...

--- src/app/components/setup-donation/setup-donation.component.spec.ts
+++ src/app/components/setup-donation/setup-donation.component.spec.ts
@@ -5,10 +5,14 @@ import {RouterTestingModule} from "@angular/router/testing";
 import {ActivatedRoute} from "@angular/router";
 import {AmountData, IncomingOrganisation} from "../../models/models";
 import {AmountsComponent} from "./amount/amounts.component";
+import { asNativeElements, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('SetupDonationComponent', () => {
   let component: SetupDonationComponent;
   let fixture: ComponentFixture<SetupDonationComponent>;
+  let elem: HTMLButtonElement;
+  let debugel: DebugElement;
 
 
   beforeEach(async () => {
@@ -48,4 +52,27 @@ describe('SetupDonationComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it('should disable mainGiveButton when no amount was selected/chosen', () => {
+    const fixture = TestBed.createComponent(SetupDonationComponent);
+    fixture.detectChanges();
+    expect(component.mainGiveButtonDisabled).toBe(true);
+    //fixture.submit();
+
+  });
+
+  it('should disable mainGiveButton when no amount was selected/chosen', () => {
+    const fixture = TestBed.createComponent(SetupDonationComponent);
+    fixture.componentInstance.mainGiveButtonDisabled = true
+    fixture.detectChanges();
+    debugel = fixture.debugElement.query(By.css('.donate-button'));
+    elem = debugel.nativeElement;
+        expect(elem.disabled).toBe(true);
+    fixture.componentInstance.mainGiveButtonDisabled = false
+    fixture.detectChanges();
+    elem = debugel.nativeElement;
+        expect(elem.disabled).toBe(false);
+    
+  });
+
 });
