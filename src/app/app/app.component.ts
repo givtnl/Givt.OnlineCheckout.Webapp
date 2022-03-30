2062ea0d8750277ac63bd7f5a2090ead863e1ce7
Mike <mike@givtapp.net>
2022-03-30 18:09:50 +0200

    Added local storage service and http interceptor

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -1,12 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 
+import { LocalStorageService } from "./../core/core.module"
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
+  constructor(private storageService: LocalStorageService) { }
   ngOnInit(): void {
+    this.storageService.testLocalStorage();
   }
-
 }
