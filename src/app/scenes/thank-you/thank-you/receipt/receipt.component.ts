8944cba68bfeae4768b469877c3bc4e8b3f76b04
michiel46 <michielverkest@gmail.com>
2022-03-31 14:11:24 +0200

    added the receipt html and css + frontend validation of the email

--- src/app/scenes/thank-you/thank-you/receipt/receipt.component.ts
+++ src/app/scenes/thank-you/thank-you/receipt/receipt.component.ts
@@ -1,35 +1,42 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
-  selector: 'app-receipt',
-  templateUrl: './receipt.component.html',
-  styleUrls: ['./receipt.component.scss']
+    selector: 'app-receipt',
+    templateUrl: './receipt.component.html',
+    styleUrls: ['./receipt.component.scss']
 })
 export class ReceiptComponent implements OnInit {
-  public emailForm!: FormGroup;
-  emailShown: boolean = false;
-  @Output()
-  emailShownChanged = new EventEmitter<boolean>()
+    email = "";
+    invalidEmail = false;
+    emailFormShown: boolean = false;
+    @Output()
+    onEmailSubmit = new EventEmitter<string>();
 
-  @Output()
-  emailChanged = new EventEmitter<string>()
+    constructor() { }
 
-  constructor(private formBuilder: FormBuilder) { }
-  
+    ngOnInit(): void { }
 
-  ngOnInit(): void {
-    this.emailForm = this.formBuilder.group({
-      email: ''
-    })
-  }
+    showEmailForm() {
+        this.emailFormShown = true;
+    }
 
-  toggleEmail() {
-    this.emailShown = !this.emailShown
-    this.emailShownChanged.emit(this.emailShown)
-  }
+    submitEmail() {
+        if (ReceiptComponent.isValidEmail(this.email)) {
+            this.emailFormShown = false
+            this.onEmailSubmit.emit(this.email)
+        } else {
+            this.invalidEmail = true;
+        }
+    }
 
-  emailChangedListener(event: any) {
-    this.emailChanged.emit(event.target.value)
-  }
+    emailChanged($event: any) {
+        if (this.invalidEmail) this.invalidEmail = false;
+        this.email = $event.target.value;
+    }
+
+    private static isValidEmail(email: string): boolean {
+        let regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        return regexp.test(email.trim())
+    }
 }
--- src/app/scenes/thank-you/thank-you/thank-you.component.ts
+++ src/app/scenes/thank-you/thank-you/thank-you.component.ts
@@ -43,9 +43,7 @@ export class ThankYouComponent implements OnInit {
     localStorage.setItem('email', this.email!.trim());
   }
 
-
-  private static isValidEmail(email: string): boolean {
-    let regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-    return regexp.test(email.trim())
-  }
+    sendEmail($event: string) {
+        this.userWantsReceipt = false
+    }
 }
