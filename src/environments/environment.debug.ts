a1b0cc20fbbf24a9f6267de2a55b960a0d1d5329
michiel46 <michielverkest@gmail.com>
2022-04-12 12:24:59 +0200

    trying the return url without hash

--- src/environments/environment.debug.ts
+++ src/environments/environment.debug.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'https://givtonlinecheckoutapidebug.azurewebsites.net',
-  returnUrl: 'https://givtonlinecheckoutwebappdebug.azurewebsites.net/#/result/success'
+  returnUrl: 'https://givtonlinecheckoutwebappdebug.azurewebsites.net/result/success'
 }
