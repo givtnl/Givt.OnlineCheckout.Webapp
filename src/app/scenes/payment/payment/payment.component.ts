ed06643a7ac5fc115379c0af3a9ee0d1a2714183
michiel46 <michielverkest@gmail.com>
2022-04-13 15:18:11 +0200

    add idealbank payment method

--- src/app/scenes/payment/payment/payment.component.ts
+++ src/app/scenes/payment/payment/payment.component.ts
@@ -16,6 +16,7 @@ export class PaymentComponent implements OnInit {
     organisationName!: string
     logoUrl!: string
     stripe: any;
+    idealBank: any;
 
     elementsOptions: StripeElementsOptions = {
         locale: 'nl',
@@ -26,14 +27,31 @@ export class PaymentComponent implements OnInit {
         }
     };
 
+    idealElementsOptions = {
+        style: {
+            base: {
+                padding: '10px 12px',
+                color: '#32325d',
+                fontSize: '16px',
+                '::placeholder': {
+                    color: '#aab7c4'
+                },
+            },
+        },
+    };
+
     constructor(private router: Router, private route: ActivatedRoute, public loader: LoadingService) {
     }
 
     initializeStripe(): void {
         this.stripe = window.Stripe!("pk_test_51HmwjvLgFatYzb8pQD7L83GIWCjeNoM08EgF7PlbsDFDHrXR9dbwkxRy2he5kCnmyLuFMSolwgx8xmlmJf5mr33200V44g2q5P");
         const elements = this.stripe.elements(this.elementsOptions)
+
         const paymentElement = elements.create("payment");
         paymentElement.mount("#payment-element");
+
+        this.idealBank = elements.create("idealBank", this.idealElementsOptions)
+        this.idealBank.mount('#ideal-bank-element')
     }
 
     ngOnInit(): void {
