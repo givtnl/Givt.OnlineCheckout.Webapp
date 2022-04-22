17d56cbb5f10d56d491dd36c83d0eae26f4a45ed
michiel46 <michielverkest@gmail.com>
2022-04-22 12:33:15 +0200

    the final solution

--- src/environments/environment.debug.ts
+++ src/environments/environment.debug.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'https://goc-debug-api-app-service.azurewebsites.net',
-  returnUrl: 'https://goc-debug-web-app-app-service.azurewebsites.net/#/result/'
+  returnUrl: 'https://goc-debug-web-app-app-service.azurewebsites.net/result/'
 }
