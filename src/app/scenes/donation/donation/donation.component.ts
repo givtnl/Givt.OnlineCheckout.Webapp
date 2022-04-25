60fd1bbaf893371327c9b8f351e97e4cbcf17567
michiel46 <michielverkest@gmail.com>
2022-04-25 14:34:58 +0200

    better solution: only wait the first time

--- src/app/scenes/donation/donation/donation.component.ts
+++ src/app/scenes/donation/donation/donation.component.ts
@@ -4,7 +4,6 @@ import AmountData from 'src/app/shared/models/donations/amount-data';
 import Organisation from 'src/app/shared/models/organisations/organisation';
 import PaymentMethod from 'src/app/shared/models/payment-methods/payment-method';
 import {LoadingService} from "../../../core/services/loading.service";
-import {StripeElementLocale, StripeElementsOptions} from "@stripe/stripe-js";
 import PaymentIntent from "../../../shared/models/payment-intent/payment-intent";
 import {environment} from "../../../../environments/environment";
 import {HttpClient} from "@angular/common/http";
@@ -24,6 +23,7 @@ export class DonationComponent implements OnInit {
     email = '';
     loading$ = this.loader.loading$;
     walletPossible = false;
+    callToCanUseWalletDone = false;
 
     paymentRequestButton: any;
     stripe: any;
@@ -37,6 +37,24 @@ export class DonationComponent implements OnInit {
         this.organisation = this.route.snapshot.data['organisation'];
         this.mainGiveButtonDisabled = true
         this.stripe = window.Stripe!("pk_test_51HmwjvLgFatYzb8pQD7L83GIWCjeNoM08EgF7PlbsDFDHrXR9dbwkxRy2he5kCnmyLuFMSolwgx8xmlmJf5mr33200V44g2q5P");
+
+        //make dummy payment request to check for wallet enableing
+        const paymentRequest = this.stripe.paymentRequest({
+            country: 'BE',
+            currency: this.organisation.currency.toLowerCase(),
+            total: {
+                label: this.organisation.name,
+                amount: 0
+            }
+        })
+
+        paymentRequest.canMakePayment().then((result: any) => {
+            console.log(result)
+            if (result) {
+                this.walletPossible = true;
+            }
+            this.callToCanUseWalletDone = true;
+        })
     }
 
     async submit() {
@@ -102,6 +120,18 @@ export class DonationComponent implements OnInit {
         return amount >= .5 && amount <= 25000;
     }
 
+    checkIfCanUseWallet() {
+        if (!this.callToCanUseWalletDone) {
+            setTimeout(() => {
+                if (this.walletPossible) {
+                    this.setupWalletPayment()
+                }
+            }, 200)
+        } else if (this.walletPossible) {
+            this.setupWalletPayment();
+        }
+    }
+
     setupWalletPayment() {
         let amount = 0;
         if (this.inputMode) {
@@ -124,36 +154,34 @@ export class DonationComponent implements OnInit {
         paymentRequest.canMakePayment().then((result: any) => {
             if (result) {
                 this.walletPossible = true;
-                setTimeout(() => {
-                    this.elements = this.stripe.elements()
-                    this.paymentRequestButton = this.elements.create('paymentRequestButton', {
-                        paymentRequest: paymentRequest
-                    })
-                    this.paymentRequestButton.mount('#payment-request-button');
+                this.elements = this.stripe.elements()
+                this.paymentRequestButton = this.elements.create('paymentRequestButton', {
+                    paymentRequest: paymentRequest
+                })
+                this.paymentRequestButton.mount('#payment-request-button');
 
-                    paymentRequest.on('paymentmethod', (ev: any) => {
-                            this.http.post<PaymentIntent>(environment.apiUrl + '/api/donation/intent', {
-                                "amount": amount,
-                                "medium": this.organisation.id,
-                                "paymentMethod": 'googlepay',
-                                "timezoneOffset": new Date().getTimezoneOffset(),
-                                "currency": this.organisation.currency
-                            }).subscribe(pi => {
-                                localStorage.setItem('token', pi.token);
-                                this.stripe.confirmCardPayment(pi.paymentMethodId, {payment_method: ev.paymentMethod.id}, {handleActions: false})
-                                    .then((result: any) => {
-                                        if (result.error) {
-                                            ev.complete('fail');
-                                            this.router.navigate(['result', 'failure']);
-                                        } else {
-                                            ev.complete('success');
-                                            this.router.navigate(['result', 'success']);
-                                        }
-                                    })
-                            })
-                        }
-                    )
-                }, 100) //need to wait .5s so the walletPossible makes sure the element is on the DOM
+                paymentRequest.on('paymentmethod', (ev: any) => {
+                        this.http.post<PaymentIntent>(environment.apiUrl + '/api/donation/intent', {
+                            "amount": amount,
+                            "medium": this.organisation.id,
+                            "paymentMethod": 'googlepay',
+                            "timezoneOffset": new Date().getTimezoneOffset(),
+                            "currency": this.organisation.currency
+                        }).subscribe(pi => {
+                            localStorage.setItem('token', pi.token);
+                            this.stripe.confirmCardPayment(pi.paymentMethodId, {payment_method: ev.paymentMethod.id}, {handleActions: false})
+                                .then((result: any) => {
+                                    if (result.error) {
+                                        ev.complete('fail');
+                                        this.router.navigate(['result', 'failure']);
+                                    } else {
+                                        ev.complete('success');
+                                        this.router.navigate(['result', 'success']);
+                                    }
+                                })
+                        })
+                    }
+                )
             }
         })
     }
