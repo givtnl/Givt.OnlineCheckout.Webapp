9c8dd076682d25d4d235db28c3815be699463c03
Mike Pattyn <mike.pattyn1@gmail.com>
2022-10-10 16:50:31 +0200

    Remove the freakin slesh and get this working again

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://goc-production-api-app-service.azurewebsites.net/',
+    apiUrl: 'https://goc-production-api-app-service.azurewebsites.net',
     returnUrl: 'https://donate.givtapp.net/result/',
     stripePk: 'pk_live_51HmwjvLgFatYzb8ph41n4L9qOn0UaRqiQVEBHAqjdXUbwnXUqzkPMVwPABQp67T7Wa0nVXqKlFgkZc0PFMFNrEU100KVSUvI8c',
     mixpanelKey: '8371976c0774440685e12457d8f442aa',
