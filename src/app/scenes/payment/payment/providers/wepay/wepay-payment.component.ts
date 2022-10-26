4f93224600f681bce3293296273fc13a313aaf99
George Kavanagh <george@givtapp.net>
2022-10-26 09:45:58 +0200

    style we pay iframe and add loader

--- src/app/scenes/payment/payment/providers/wepay/wepay-payment.component.ts
+++ src/app/scenes/payment/payment/providers/wepay/wepay-payment.component.ts
@@ -1,21 +1,20 @@
-import { AfterViewInit, Component, Inject, Input, OnInit} from '@angular/core';
+import { AfterViewInit, Component, Inject, Input, OnInit } from '@angular/core';
 import PaymentIntent from '../../../../../shared/models/payment-intent/payment-intent';
-import {LoadingService} from "../../../../../core/services/loading.service";
-import {environment} from "../../../../../../environments/environment";
-import { DOCUMENT } from '@angular/common'; 
-import {HttpClient} from "@angular/common/http";
-import { AngularWePayService } from '@givtnl/angular-wepay-service'
+import { LoadingService } from '../../../../../core/services/loading.service';
+import { environment } from '../../../../../../environments/environment';
+import { DOCUMENT } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { AngularWePayService } from '@givtnl/angular-wepay-service';
 
-const apiVersion = "3.0";
-const iframeContainerId = "credit-card-iframe";
+const apiVersion = '3.0';
+const iframeContainerId = 'credit-card-iframe';
 
 @Component({
     selector: 'payment-wepay',
     templateUrl: './wepay-payment.component.html',
-    styleUrls: ['./wepay-payment.component.scss']
+    styleUrls: ['./wepay-payment.component.scss'],
 })
 export class WepayPaymentComponent implements OnInit, AfterViewInit {
-
     @Input() paymentMethod: PaymentIntent | undefined;
     clientSelectedPaymentMethod!: string;
     loading$ = this.loader.loading$;
@@ -24,49 +23,52 @@ export class WepayPaymentComponent implements OnInit, AfterViewInit {
     fullName: string | undefined;
     zipCode: string | undefined;
 
+    constructor(
+        private wePayService: AngularWePayService,
+        public loader: LoadingService,
+        @Inject(DOCUMENT) document: Document,
+        private http: HttpClient
+    ) {}
 
-    constructor(private wePayService: AngularWePayService, public loader: LoadingService, @Inject(DOCUMENT) document: Document, private http: HttpClient) { }
-
-    ngOnInit(): void { 
-        this.clientSelectedPaymentMethod = localStorage.getItem('paymentMethod')!
-
+    ngOnInit(): void {
+        this.clientSelectedPaymentMethod =
+            localStorage.getItem('paymentMethod')!;
     }
 
     ngAfterViewInit(): void {
-
         const custom_style = {
-            'styles': {
+            styles: {
                 'cvv-icon': {
-                    'base': {
-                        'display': 'none'
-                    }
+                    base: {
+                        display: 'none',
+                    },
                 },
-                'base': {
-                    'height': '44px',
-                    'margin': '0',
-                    'border-radius': '4px',
+                base: {
+                    height: '40px',
+                    margin: '0',
+                    'border-radius': '0.25rem',
                     'font-family': 'Avenir',
                     'font-size': '16px',
-                    'color': '#2C2B57',
+                    color: '#2C2B57',
                     '::placeholder': {
-                        'color': '#BCB9C8'
+                        color: '#BCB9C8',
                     },
                     ':focus': {
-                        'border': '1px solid #2C2B57'
-                    }
+                        border: '1px solid #2C2B57',
+                    },
                 },
-                'invalid': {
-                    'border': '2px solid #D73C49'
+                invalid: {
+                    border: '2px solid #D73C49',
                 },
-                'valid': {
-                    'border': '1px solid #41C98E'
+                valid: {
+                    border: '1px solid #41C98E',
                 },
-                'errors': {
-                    'invalid': {
-                        'color': '#D73C49'
-                    }
-                }
-            }
+                errors: {
+                    invalid: {
+                        color: '#D73C49',
+                    },
+                },
+            },
         };
 
         const options = {
@@ -74,58 +76,61 @@ export class WepayPaymentComponent implements OnInit, AfterViewInit {
             show_labels: false,
             show_placeholders: true,
             show_error_messages: false,
-            show_error_messages_when_unfocused: false
+            show_error_messages_when_unfocused: false,
         };
 
-        this.wePayService.create().then(
-            wepay => {
-                this.wepay = wepay;
+        this.wePayService.create().then((wepay) => {
+            this.loader.show();
+            this.wepay = wepay;
 
-                // javascript library docs: https://dev.wepay.com/sdks-and-libraries/helper-js/
-                // https://dev.wepay.com/clear/create-payment-methods/
-                // https://dev.wepay.com/clear/cookbooks/style-credit-card-iframes/
+            // javascript library docs: https://dev.wepay.com/sdks-and-libraries/helper-js/
+            // https://dev.wepay.com/clear/create-payment-methods/
+            // https://dev.wepay.com/clear/cookbooks/style-credit-card-iframes/
 
-                // The non minimized library can be found here: https://cdn.wepay.com/wepay.full.js
+            // The non minimized library can be found here: https://cdn.wepay.com/wepay.full.js
 
-                let error = wepay.configure(
-                    environment.production ? "production" : "stage",
-                    environment.wePayAppId, apiVersion);
-                
-                if (error) {
-                    console.log(error)
-                };
+            let error = wepay.configure(
+                environment.production ? 'production' : 'stage',
+                environment.wePayAppId,
+                apiVersion
+            );
 
-
-                this.creditCard = wepay.createCreditCardIframe(iframeContainerId, null);
-                document.getElementById(iframeContainerId)?.children[0].addEventListener('load', function() {
-                    console.log("Finished loading iframe!");
-                    // TODO
-                    // set iframeReady = true and hide the other form elements till ready
-                    // Add loader ??? 
-                })
+            if (error) {
+                console.log(error);
             }
-        )
+
+            this.creditCard = wepay.createCreditCardIframe(
+                iframeContainerId,
+                options
+            );
+
+            let that = this;
+            document
+                .getElementById(iframeContainerId)
+                ?.children[0].addEventListener('load', function () {
+                    that.loader.hide();
+                });
+        });
     }
 
+    confirmCardPayment() {
+        this.loader.show();
 
-  confirmCardPayment() {
-    this.loader.show();
+        const tokenizeDetails = {
+            holder_name: this.fullName,
+            address: {
+                postal_code: this.zipCode,
+            },
+        };
 
-    const tokenizeDetails = {
-         "holder_name": this.fullName,
-         "address": {
-             "postal_code": this.zipCode
-        }
-    }; 
+        this.creditCard
+            .tokenize(tokenizeDetails)
+            .then((response: any) => {
+                console.log(response.id);
 
-    console.log(tokenizeDetails);
-
-    this.creditCard.tokenize(tokenizeDetails).then( (response: any) => {
-        console.log(response.id);
-
-        // Story ends here. 
-        // Backend I/O not needed for this story!
-        /*
+                // Story ends here.
+                // Backend I/O not needed for this story!
+                /*
         this.http.post(environment.apiUrl + '/api/donation/payment', {
             "currency": localStorage.getItem('organisationCurrency'),
             "amount": localStorage.getItem('amount'),
@@ -138,13 +143,13 @@ export class WepayPaymentComponent implements OnInit, AfterViewInit {
             console.log(data);
         })
         */
-    })
-    .catch(function(error:any) {
-        console.log(error);
-        // TODO input validation should be managed here
-        // TODO error mgmt
-    })
+            })
+            .catch(function (error: any) {
+                console.log(error);
+                // TODO input validation should be managed here
+                // TODO error mgmt
+            });
 
-    this.loader.hide();
-  }
+        this.loader.hide();
+    }
 }
