fea73899f2753040a8459d9898db77159e87f165
George Kavanagh <george@givtapp.net>
2022-10-27 18:09:58 +0200

    fixed organisationCountry bug

--- src/app/scenes/payment/payment/payment.component.ts
+++ src/app/scenes/payment/payment/payment.component.ts
@@ -1,33 +1,40 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import PaymentIntent from '../../../shared/models/payment-intent/payment-intent';
-import {LoadingService} from "../../../core/services/loading.service";
+import { LoadingService } from '../../../core/services/loading.service';
 import mixpanel from 'mixpanel-browser';
-import {Title} from "@angular/platform-browser";
-import {TranslateService} from "@ngx-translate/core";
-import { environment } from "../../../../environments/environment";
-
+import { Title } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
+import { environment } from '../../../../environments/environment';
 
 @Component({
     selector: 'app-payment',
     templateUrl: './payment.component.html',
-    styleUrls: ['./payment.component.scss']
+    styleUrls: ['./payment.component.scss'],
 })
 export class PaymentComponent implements OnInit {
-    paymentMethod: PaymentIntent | undefined
+    paymentMethod: PaymentIntent | undefined;
     loading$ = this.loader.loading$;
-    title!: string
-    logoUrl!: string
-    paymentProvider: string | null;
+    title!: string;
+    logoUrl!: string;
+    organisationCountry!: string;
 
-
-    constructor(private router: Router, private route: ActivatedRoute, public loader: LoadingService, private pageTitle: Title, private translate: TranslateService) {
-        this.paymentProvider = localStorage.getItem('paymentProvider');
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute,
+        public loader: LoadingService,
+        private pageTitle: Title,
+        private translate: TranslateService
+    ) {
+        this.organisationCountry = localStorage.getItem('organisationCountry')!;
     }
 
     ngOnInit(): void {
-        this.pageTitle.setTitle(this.translate.instant('Page.Title'))
-        mixpanel.track('page_load', {page: 'payment_page', organisationName: localStorage.getItem('organisationName')});
+        this.pageTitle.setTitle(this.translate.instant('Page.Title'));
+        mixpanel.track('page_load', {
+            page: 'payment_page',
+            organisationName: localStorage.getItem('organisationName'),
+        });
         this.paymentMethod = this.route.snapshot.data['donation'];
         this.title = localStorage.getItem('organisationTitle')!;
         this.logoUrl = localStorage.getItem('logoUrl')!;
